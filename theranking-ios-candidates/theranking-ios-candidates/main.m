45022edb9ab0e477d781f9a99b64ba7686126633
Antonio Jimenez <antonio.jimenez2@gmail.com>
2015-03-10 11:17:53 +0100

    Initial project commit

---
+++ theranking-ios-candidates/theranking-ios-candidates/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  theranking-ios-candidates
+//
+//  Created by ANTONIO JIMÉNEZ MARTÍNEZ on 10/3/15.
+//  Copyright (c) 2015 antjimar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
