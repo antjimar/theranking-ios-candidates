beca3c4c22f6850a224d78cc2b35e31a38aead4b
Antonio Jimenez <antonio.jimenez2@gmail.com>
2015-03-12 11:00:41 +0100

    Remove unused imports

--- theranking-ios-candidates/theranking-ios-candidates/ViewControllers/MainPictureViewController.m
+++ theranking-ios-candidates/theranking-ios-candidates/ViewControllers/MainPictureViewController.m
@@ -6,13 +6,11 @@
 //  Copyright (c) 2015 antjimar. All rights reserved.
 //
 
+#import <SDWebImage/UIImageView+WebCache.h>
 #import "MainPictureViewController.h"
-#import "PictureCollectionViewCell.h"
-#import "PictureEntity+Builder.h"
 #import "LoadPicturesInteractor.h"
 #import "LoadPictureDetailsInteractor.h"
 #import "DetailPictureViewController.h"
-#import <SDWebImage/UIImageView+WebCache.h>
 #import "PictureEntityProtocol.h"
 #import "PictureEntityCellProtocol.h"
 #import "PictureEntityFactory.h"
